78ac73b0505cf4b26f209a8b5d295960e08b8f81
anancds <anancds@163.com>
2020-10-03 17:18:18 +0800

    added pair and tupe

---
+++ functional/tupe-example.cpp
@@ -0,0 +1,31 @@
+//
+// Created by cds on 2020/10/3.
+//
+#include <functional>
+#include <iostream>
+#include <string>
+#include <tuple>
+
+int main() {
+  std::string names[3] = {"alice", "bob", "carl"};
+  char ranks[3] = {'A', 'B', 'C'};
+  int score[3] = {5, 6, 7};
+
+  auto student0 = std::make_tuple(names[0], ranks[0], score[0]);
+  std::cout << "student0> name: " << std::get<0>(student0) << ", rank: " << std::get<1>(student0)
+            << ", score: " << std::get<2>(student0) << std::endl;
+
+  std::string hoge;
+  char piyo;
+  int fuga;
+  auto student1 = std::make_tuple(names[1], ranks[1], score[1]);
+  std::tie(hoge, piyo, fuga) = student1;
+  std::cout << "student1> name: " << hoge << ", rank: " << piyo << ", score: " << fuga << std::endl;
+  std::tie(hoge, std::ignore, fuga) = student1;
+  std::cout << "student1> name: " << hoge << ", rank: " << piyo << ", score: " << fuga << std::endl;
+
+  auto student2 = std::tie(names[2], ranks[2], score[2]);
+  auto [a, b, c] = student2;  // C++17 structured binding
+  std::cout << "student2> name: " << a << ", rank: " << b << ", score: " << c << std::endl;
+  return 0;
+}
